4737b2007b9548286be7109e6083771e20842c52
Nikita <pyshchev.nikita@gmail.com>
2018-01-14 14:36:07 +0300

    New lines today

--- src/DataBase.java
+++ src/DataBase.java
@@ -1,26 +1,19 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
+import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import javax.swing.JTextField;
-import javax.swing.JTextArea;
-import javax.swing.JLabel;
-import javax.swing.JComboBox;
 
-public class DataBase extends JFrame{
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+public class DataBase extends JFrame implements ActionListener{
 	
 	JPanel p=new JPanel();
 	JButton b=new JButton("Start");
 	JTextField t=new JTextField("Go!",20);
-	JTextArea ta=new JTextArea("How are you",20,5);
-	JLabel l=new JLabel("What's up");
-	String choices[]={
-		"Hallo",
-		"Bonjour",
-		"gggg"
-	};
-	JComboBox cb=new JComboBox(choices);
-	
+		
 	public static void main(String[] args){
 		new DataBase();
 	}
@@ -28,16 +21,19 @@ public class DataBase extends JFrame{
 	public DataBase(){
 	super("DataBase");
 	
-		setSize(800,300);
+		setSize(400,200);
 		setResizable(true);
-		
+		b.addActionListener(this);
 		p.add(b);
 		p.add(t);
-		p.add(ta);
-		p.add(l);
-		p.add(cb);
 		add(p);
-		
+
 		setVisible(true);
 	}
+	
+	public void actionPerformed(ActionEvent e){
+		JOptionPane.showMessageDialog(null, t.getText());
+		
+	};
 }
+
